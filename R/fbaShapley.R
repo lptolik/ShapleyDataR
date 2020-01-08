a5675ad06f20907fd3f9f1ccb0cfd2bfdc6a151f
Anatoly Anatoly <lptolik@gmail.com>
2020-01-08 13:13:58 +0000

    Shapley values for FBA models

---
+++ R/fbaShapley.R
@@ -0,0 +1,14 @@
+#' Shapley values for FBA model.
+#'
+#' @param mod FBA model in Sybil format.
+#' @param reId list of reaction Ids to be used in calculations.
+#' @param tol tolerance for Shapley value convergence
+#' @param logName name of the file to append log records to
+#' @param tmpSave frequency of tmp results saving
+#'
+#' @return
+#' @export
+#'
+fbaShapley<-function(mod,reId,tol=0.05,logName='fbaShapley.log',tmpSave=500){
+
+}
