efd00c84f5059522d04661668da5fdbadb4e3db8
Anatoly Anatoly <lptolik@gmail.com>
2020-01-08 13:56:54 +0000

    first variant of fbaShapley NOT tested

--- R/fbaShapley.R
+++ R/fbaShapley.R
@@ -1,14 +1,88 @@
 #' Shapley values for FBA model.
 #'
+#' \code{fbaShapley} return object of class \code{shapley}, which contains
+#' \describe{
+#'   \item{reId}{Reaction IDs as provided by argument}
+#'   \item{shapley}{Shapley values, vector with reId as names}
+#'   \item{sd}{Standard deviation for the Shapley values}
+#'   \item{p}{Probability that the \code{shapley} contains real Shapley values.}
+#' }
+#' for reactions, their standard deviations,
 #' @param mod FBA model in Sybil format.
 #' @param reId list of reaction Ids to be used in calculations.
 #' @param tol tolerance for Shapley value convergence
 #' @param logName name of the file to append log records to
 #' @param tmpSave frequency of tmp results saving
+#' @param perfTolerance tolerance for fail early check
 #'
-#' @return
+#' @return object of class \code{shapley}
 #' @export
 #'
-fbaShapley<-function(mod,reId,tol=0.05,logName='fbaShapley.log',tmpSave=500){
-
+#' @importFrom sybil optimizeProb
+fbaShapley<-function(mod,reId,tol=0.05,logName='fbaShapley.log',tmpSave=500,perfTolerance=1e-5){
+  convTol<-0.1
+  N<-length(reId)
+  tmpFile<-paste0('fbaShapley.',format(Sys.time(), "%Y%m%d%H%M"),'.RData')
+  phi<-list()
+  sd<-list()
+  val<-list()
+  permL<-list()
+  jStop<-c()
+  mTot<- optimizeProb(mod,algorithm = "fba",retOptSol = FALSE)
+  vTot<-mTot$obj
+  v<-rep(0.0,N)
+  vNull<-0
+  t<-1
+  phi[[1]]<-rep(0.0,N)
+  val[[1]]<-rep(0.0,N)
+  sd[[1]]<-rep(0.0,N)
+  while(!convCriteria(phi,convTol)){
+    t<-t+1
+    if(t<=100){
+      cat(format(Sys.time(), "%b %d %X"),'t=',t,'\n',append = TRUE,file = logName)
+    }else{
+      tolV<-sum(abs(phi[[t-1]]-phi[[t-100]])/(1e-5+abs(phi[[t-1]])))
+      cat(format(Sys.time(), "%b %d %X"),'t=',t,'tol=',tolV,'\n',append = TRUE,file = logName)
+      if(t%%tmpSave==0){
+        save(phi,t,N,vTot,v,val,permL,sd,perfTolerance,vNull,tolV,mod,reId,jStop,file = tmpFile)
+      }
+    }
+    perm<-makePerm(N)
+    v<-rep(0.0,N)
+    newRes<-vTot
+    belowIdx<-0
+    for (j in (1:N)){
+      oldRes<-newRes
+      bound<-rep(0,j)
+      model<-optimizeProb(mod,algorithm = "fba",retOptSol = FALSE,react=reId[perm[1:j]],lb=bound,ub=bound)
+      newRes<-model$obj
+      if(abs(vNull-newRes)< perfTolerance){
+        belowIdx<-belowIdx+1
+      }else{
+        belowIdx<-0
+      }
+      if(belowIdx>5){
+        v[j:N]<-0
+        jStop[t]<-j
+        break()
+      }
+      v[j]<-newRes-oldRes
+    }
+    val[[t]]<-rep(0.0,N)
+    val[[t]]<-v
+    permL[[t]]<-perm
+    phi[[t]]<-rep(0.0,N)
+    phi[[t]][perm]<-phi[[t-1]][perm]+(v-phi[[t-1]][perm])/t
+    sd[[t]]<-rep(0.0,N)
+    sd[[t]][perm]<-sd[[t]][perm]+(v-phi[[t-1]][perm])*(v-phi[[t]][perm])
+  }
+  save(phi,t,N,vTot,v,val,permL,sd,perfTolerance,vNull,tolV,mod,reId,jStop,file = tmpFile)
+# TODO: calculate probability for the SHapley data
+  shpl<--1*phi[[t]]
+  res<-list(reId=reId,
+            shapley=shpl,
+            sd=sd[[t]],
+            p=NA)
+  class(res)<-'shapley'
+  return(res)
 }
