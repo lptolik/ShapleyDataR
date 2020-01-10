f8f58a410f7d222a2fd1a8461edf1531f7fe5db6
Anatoly Anatoly <lptolik@gmail.com>
2020-01-10 14:08:43 +0000

    explicitly set some Shapley value to zero during import

--- R/extractShapleyfromLog.R
+++ R/extractShapleyfromLog.R
@@ -1,14 +1,16 @@
 #' Extract Shapley data from temporary savings
 #'
 #' @param fname temporary save file to extract data from
+#' @param tol the shapley close to zero will be set to zero explicitly
 #'
 #' @return  data.frame having the same column names as properties of shapley class.
 #' @export
 
-extractShapley<-function(fname){
+extractShapley<-function(fname,tol=1e-12){
   e1 <- new.env(parent = baseenv())
 load(fname,envir = e1)
 phiLast<-e1$phi[[length(e1$phi)]]
+phiLast[abs(phiLast)<tol]<-0
 idx<-match(e1$reId,e1$mod@react_id)
 phiDF<-data.frame(id=e1$reId,
                   name=e1$mod@react_name[idx],
