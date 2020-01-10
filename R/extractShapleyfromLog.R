6816dea46a6388e21f186bb25633f0ccf841f8c4
Anatoly Anatoly <lptolik@gmail.com>
2020-01-10 14:37:44 +0000

    norm the shapley to the total performance, so they can be compared

--- R/extractShapleyfromLog.R
+++ R/extractShapleyfromLog.R
@@ -6,7 +6,7 @@
 #' @return  data.frame having the same column names as properties of shapley class.
 #' @export
 
-extractShapley<-function(fname,tol=1e-12){
+extractShapley<-function(fname,tol=1e-15){
   e1 <- new.env(parent = baseenv())
 load(fname,envir = e1)
 phiLast<-e1$phi[[length(e1$phi)]]
@@ -15,6 +15,7 @@ idx<-match(e1$reId,e1$mod@react_id)
 phiDF<-data.frame(id=e1$reId,
                   name=e1$mod@react_name[idx],
                   shapley=-1*phiLast,
+                  shNorm=-1*phiLast/e1$vTot,
                   sd=e1$sd,
                   err01=e1$e[,1],
                   err05=e1$e[,2],
